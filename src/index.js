047da944a0c5c7cb3b967346fc9370289d4022cc
Kirill <kirill.tovpinets@gmail.com>
2018-02-22 16:05:05 +0300

    19 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	if (base > 10) {
+		devider = Math.round(base / 2);
+	}else{
+		devider = 5;
+	}
+  var floor = Math.floor(number / devider);
+	var numZeroes = 0;
+	while(floor !== 0){
+		numZeroes += floor;
+		floor = Math.floor(floor / devider);
+	}
+  return numZeroes;
 }
